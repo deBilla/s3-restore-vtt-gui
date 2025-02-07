f8d6a2ad69fa63be77c7cbaba1ff7867d0c6d03a
Dimuthu Wickaramanayake <dimuthu.billa@gmail.com>
2025-02-07 10:43:34 +0800

    Update configurations.ts

--- src/core/configurations.ts
+++ src/core/configurations.ts
@@ -3,8 +3,8 @@ export default () => ({
   s3: {
     bucket: {
       media: {
-        name: 'qalbox-media-ibm-aspera',
-        prefix: 'workspaces/84394/home/qalbox.media@bitsmedia.com (490177)/qalbox/'
+        name: <>,
+        prefix: <>'
       }
     }
   },
